bde44914fbf4c0cdcadc7bbe99bc1c01a0a611d8
hannybear88 <hannahkchung88@gmail.com>
2023-02-13 20:30:02 -0800

    modified client/src/js/datahbase.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,27 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => { console.error('putDb not implemented');
+const jateDb = await openDB("jate", 1);
+const tx = jateDb.transaction("jate", "readwrite");
+const store = tx.objectStore("jate");
+const request = store.put({ id:1,  content: content });
+const result = await request;
+  console.log("🚀 - data saved to the database", result);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Add logic for a method that gets all the content from the database
+export const getDb = async () =>{ console.error('getDb not implemented');
+const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.getAll();
+  const result = await request;
+  if(result.length <1)
+  {return}
+  return result[0].content
+ };
+
 
 initdb();
