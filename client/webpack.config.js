6f8cafd2156641b923cb64e832d4cacfbb96fb74
hannybear88 <hannahkchung88@gmail.com>
2023-02-14 16:38:16 -0800

    modified client/dist/install.bundle.js, main.bundle.js, src-sw.js, index.html, database.js, install.js, webpack.config.js files, and moved images in Assets folder to assets folder and deleted Assets folder

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Text Editor'
+        title: 'Progressive Web Application Text Editor'
       }),
 
       new InjectManifest({
